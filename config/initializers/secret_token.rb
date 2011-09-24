f322677410fcd0dc23302e80f4a5044dcb695851
Luke Mcildoon <luke@twofiftyfive.net>
2011-09-24 17:46:41 +1000

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedisTest::Application.config.secret_token = 'a5a536ce48248f4b70ae90ad2ad7853b25117022d1705073a484e3e6961f103924ef154424b3976e1386643181b6cf120ff12bc6e5d48a542215ac286447843b'
